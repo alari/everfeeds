daf952f3f3514f7c2db36a837af21603ec068200
Dmitry Kurinskiy <name.alari@gmail.com>
2011-03-20 14:05:19 +0300

    log4j

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -62,21 +62,42 @@ environments {
 
 }
 
-// log4j configuration
+/**
+* Directory configuration.
+* Pickup the Tomcat/Catalina directory else use the target or current dir.
+*/
+def fs = File.separator // Local variable.
+globalDirs.targetDir = new File("target${fs}").isDirectory() ? "target${fs}" : ''
+globalDirs.catalinaBase = System.properties.getProperty('catalina.base')
+globalDirs.logDirectory = globalDirs.catalinaBase ? "${globalDirs.catalinaBase}${fs}logs${fs}" : globalDirs.targetDir
+globalDirs.workDirectory = globalDirs.catalinaBase ? "${globalDirs.catalinaBase}${fs}work${fs}" : globalDirs.targetDir
+globalDirs.searchableIndexDirectory = "${globalDirs.workDirectory}SearchableIndex${fs}${appName}${fs}"
+
+/**
+ * Log4j configuration.
+ * Causing this file to reload (e.g. edit+save) may break the appLog destination
+ * and further logs will be written to files or directories like "[:]".
+ * For more info see http://logging.apache.org/log4j/1.2/manual.html
+ * For log levels see http://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/Level.html
+ * Basic log levels are ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
+ */
 log4j = {
     appenders {
-        rollingFile  name:'infoLog', file:'info.log', threshold: org.apache.log4j.Level.INFO, maxFileSize:1024
-        rollingFile  name:'warnLog', file:'warn.log', threshold: org.apache.log4j.Level.WARN, maxFileSize:1024
-        rollingFile  name:'errorLog', file:'error.log', threshold: org.apache.log4j.Level.ERROR, maxFileSize:1024
-        rollingFile  name:'custom', file:'custom.log', maxFileSize:1024
+        // Use if we want to prevent creation of a stacktrace.log file.
+        'null' name:'stacktrace'
+
+        // Use this if we want to modify the default appender called 'stdout'.
+        console name:'stdout', layout:pattern(conversionPattern: '[%t] %-5p %c{2} %x - %m%n')
+
+        // Custom log file.
+        rollingFile name:"appLog",
+                        file:"${globalDirs.logDirectory}${appName}.log".toString(),
+                        maxFileSize:'300kB',
+                        maxBackupIndex:1,
+                        layout:pattern(conversionPattern: '%d{[EEE, dd-MMM-yyyy @ HH:mm:ss.SSS]} [%t] %-5p %c %x - %m%n')
     }
 
-    root {
-        info 'infoLog','warnLog','errorLog','custom', stdout
-        error()
-        additivity = true
-    }
-
+    // This is for the built-in stuff and from the default Grails-1.2.1 config.
     error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
             'org.codehaus.groovy.grails.web.pages', //  GSP
             'org.codehaus.groovy.grails.web.sitemesh', //  layouts
@@ -89,7 +110,53 @@ log4j = {
             'org.hibernate',
             'net.sf.ehcache.hibernate'
 
-    warn 'org.mortbay.log'
+    warn   'org.mortbay.log' // Jetty
+
+    error 'grails.app' // Set the default log level for our app code.
+    info 'grails.app.bootstrap' // Set the log level per type and per type.class
+    error 'grails.app.service.AuthService'
+    error 'grails.app.service.NavigationService'
+    error 'grails.app.service.com.zeddware.grails.plugins.filterpane.FilterService'
+    info 'org.codehaus.groovy.grails.plugins.searchable'
+    //info 'org.compass'
+    error 'grails.app.task' // Quartz jobs.
+    info 'grails.app.task.InventoryIndexJob'
+
+    // Move anything that should behave differently into this section.
+    switch(environment) {
+        case 'development':
+            // Configure the root logger to output to stdout and appLog appenders.
+            root {
+                error 'stdout','appLog'
+                additivity = true
+            }
+            //debug "org.hibernate.SQL"
+            debug 'grails.app.service'
+            debug 'grails.app.controller'
+            break
+        case 'test':
+            // Configure the root logger to only output to appLog appender.
+            root {
+                error 'stdout','appLog'
+                additivity = true
+            }
+            debug 'grails.app.service'
+            debug 'grails.app.controller'
+            break
+        case 'production':
+            // Configure the root logger to only output to appLog appender.
+            root {
+                error 'appLog'
+                additivity = true
+            }
+            warn 'grails.app.service'
+            warn 'grails.app.controller'
+            debug 'grails.app.service.AssetCsvService'
+            debug 'grails.app.service.PersonCsvService'
+            debug 'grails.app.service.InventoryCsvService'
+            debug 'grails.app.service.AssetTreeService' /// @todo: remove after testing.
+            break
+    }
 }
 
 // Added by the Spring Security Core plugin:
