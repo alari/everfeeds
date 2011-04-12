ddd5647cb99050958c9fd56070e42e20fb15b7af
Dmitry Kurinskiy <name.alari@gmail.com>
2011-04-12 14:30:32 +0400

    tiny i18n improvements

--- src/groovy/everfeeds/I18n.groovy
+++ src/groovy/everfeeds/I18n.groovy
@@ -1,5 +1,7 @@
 package everfeeds
 
+import org.springframework.context.i18n.LocaleContextHolder as LCH
+
 /**
  * Created by alari @ 06.04.11 14:40
  *
@@ -7,4 +9,48 @@ package everfeeds
  */
 class I18n {
   static messageSource = SpringUtil.getBean("messageSource")
+
+  // Usage: I18n."code(:locale)?"([value, value?, ...]?, "Default message"?, "encode as"?)
+  static m = { String code, args ->
+
+      // Defining locale and message code
+      def locale = LCH.getLocale()
+      // TODO: get locale code from message
+
+      // Preparing things we'll get from args
+      String defaultMessage = code
+      String encodeAs = null
+
+      args = args as List
+
+      // No arguments given
+      if(args.size() == 0) {
+        args = []
+      } else {
+        List params
+        // There is a map of args and some unmapped params
+        if(args[0] instanceof List) {
+          params = args.tail()
+          args = args[0]
+        } else {
+          // Only unmapped params
+          params = args
+          args = []
+        }
+        if(params.size()) {
+          if(params[0]) defaultMessage = params[0]
+          if(params.size() > 1) {
+            encodeAs = params[1].toString().toUpperCase()
+          }
+        }
+      }
+
+      String text = I18n.messageSource.getMessage(code, args as Object[],
+              defaultMessage, locale) ?: defaultMessage
+
+      if (text) {
+        return encodeAs ? text."encodeAs${encodeAs}"() : text
+      }
+      ''
+  }
 }
--- src/groovy/everfeeds/bootstrap/I18nBoot.groovy
+++ src/groovy/everfeeds/bootstrap/I18nBoot.groovy
@@ -1,7 +1,5 @@
 package everfeeds.bootstrap
 
-import org.springframework.context.i18n.LocaleContextHolder as LCH
-
 import everfeeds.I18n
 
 /**
@@ -9,48 +7,6 @@ import everfeeds.I18n
  */
 class I18nBoot {
   static run() {
-    // Usage: I18n."code(:locale)?"([value, value?, ...]?, "Default message"?, "encode as"?)
-    I18n.metaClass.'static'.invokeMethod = { String code, args ->
-
-      // Defining locale and message code
-      def locale = LCH.getLocale()
-      // TODO: get locale code from message
-
-      // Preparing things we'll get from args
-      String defaultMessage = code
-      String encodeAs = null
-
-      args = args as List
-
-      // No arguments given
-      if(args.size() == 0) {
-        args = []
-      } else {
-        List params
-        // There is a map of args and some unmapped params
-        if(args[0] instanceof List) {
-          params = args.tail()
-          args = args[0]
-        } else {
-          // Only unmapped params
-          params = args
-          args = []
-        }
-        if(params.size()) {
-          if(params[0]) defaultMessage = params[0]
-          if(params.size() > 1) {
-            encodeAs = params[1].toString().toUpperCase()
-          }
-        }
-      }
-
-      String text = I18n.messageSource.getMessage(code, args as Object[],
-              defaultMessage, locale) ?: defaultMessage
-
-      if (text) {
-        return encodeAs ? text."encodeAs${encodeAs}"() : text
-      }
-      ''
-    }
+    I18n.metaClass.'static'.invokeMethod = { String code, args -> I18n.m.call(code, args)}
   }
 }
