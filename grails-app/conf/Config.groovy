1c4aaba364a2c5c3d5072033c59dbda808ea18a9
Dmitry Kurinskiy <name.alari@gmail.com>
2011-04-17 17:03:43 +0400

    rollingFile is back again

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,5 +1,3 @@
-import org.apache.log4j.DailyRollingFileAppender
-
 // locations to search for config files that get merged into the main config
 // config files can either be Java properties files or ConfigSlurper scripts
 
@@ -54,15 +52,15 @@ grails.views.javascript.library = "jquery"
 
 // set per-environment serverURL stem for creating absolute links
 environments {
-  production {
-    grails.serverURL = "http://everfeeds.com"
-  }
-  development {
-    grails.serverURL = "http://localhost:8080/everfeeds"
-  }
-  test {
-    grails.serverURL = "http://localhost:8080/${appName}"
-  }
+    production {
+        grails.serverURL = "http://everfeeds.com"
+    }
+    development {
+        grails.serverURL = "http://localhost:8080/everfeeds"
+    }
+    test {
+        grails.serverURL = "http://localhost:8080/${appName}"
+    }
 
 }
 
@@ -87,81 +85,81 @@ globalDirs.searchableIndexDirectory = "${globalDirs.workDirectory}SearchableInde
  * Basic log levels are ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
  */
 log4j = {
-  appenders {
-    // Use if we want to prevent creation of a stacktrace.log file.
-    'null' name: 'stacktrace'
+    appenders {
+        // Use if we want to prevent creation of a stacktrace.log file.
+        'null' name: 'stacktrace'
 
-    // Use this if we want to modify the default appender called 'stdout'.
-    console name: 'stdout', layout: pattern(conversionPattern: '[%t] %-5p %c{2} %x - %m%n')
+        // Use this if we want to modify the default appender called 'stdout'.
+        console name: 'stdout', layout: pattern(conversionPattern: '[%t] %-5p %c{2} %x - %m%n')
 
-    // Custom log file.
-    appender new DailyRollingFileAppender(
-            name: "appLog",
-            datePattern: "'.'yyyy-MM-dd",
-            fileName: "${globalDirs.logDirectory}${appName}.log".toString(),
-            layout: pattern(conversionPattern: '%d{[EEE, dd-MMM-yyyy @ HH:mm:ss.SSS]} [%t] %-5p %c %x - %m%n')
-    )
-  }
+        // Custom log file.
+        rollingFile name: "appLog",
+                file: "${globalDirs.logDirectory}${appName}.log".toString(),
+                maxFileSize: '300kB',
+                maxBackupIndex: 1,
+                layout: pattern(conversionPattern: '%d{[EEE, dd-MMM-yyyy @ HH:mm:ss.SSS]} [%t] %-5p %c %x - %m%n')
 
-  // This is for the built-in stuff and from the default Grails-1.2.1 config.
-  error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
-          'org.codehaus.groovy.grails.web.pages', //  GSP
-          'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-          'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-          'org.codehaus.groovy.grails.web.mapping', // URL mapping
-          'org.codehaus.groovy.grails.commons', // core / classloading
-          'org.codehaus.groovy.grails.plugins', // plugins
-          'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-          'org.springframework',
-          'org.hibernate',
-          'net.sf.ehcache.hibernate'
+    }
 
-  warn 'org.mortbay.log' // Jetty
+    // This is for the built-in stuff and from the default Grails-1.2.1 config.
+    error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+            'org.codehaus.groovy.grails.web.pages', //  GSP
+            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping', // URL mapping
+            'org.codehaus.groovy.grails.commons', // core / classloading
+            'org.codehaus.groovy.grails.plugins', // plugins
+            'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
 
-  debug 'everfeeds'
-  warn 'net.bull.javamelody'
+    warn 'org.mortbay.log' // Jetty
 
-  debug 'grails.app' // Set the default log level for our app code.
-  //error 'grails.app.bootstrap' // Set the log level per type and per type.class
-  //error 'grails.app.service.AuthService'
-  //error 'grails.app.service.NavigationService'
-  //error 'grails.app.service.com.zeddware.grails.plugins.filterpane.FilterService'
-  //info 'org.codehaus.groovy.grails.plugins.searchable'
-  //info 'org.compass'
-  //error 'grails.app.task' // Quartz jobs.
-  //info 'grails.app.task.InventoryIndexJob'
+    debug 'everfeeds'
+    warn 'net.bull.javamelody'
 
-  // Move anything that should behave differently into this section.
-  switch (environment) {
-    case 'development':
-      // Configure the root logger to output to stdout and appLog appenders.
-      root {
-        error 'stdout', 'appLog'
-        additivity = true
-      }
-      //debug "org.hibernate.SQL"
-      //debug 'grails.app.service'
-      //debug 'grails.app.controller'
-      break
-    case 'test':
-      // Configure the root logger to only output to appLog appender.
-      root {
-        error 'stdout', 'appLog'
-        additivity = true
-      }
-      //debug 'grails.app.service'
-      //debug 'grails.app.controller'
-      break
-    case 'production':
-      // Configure the root logger to only output to appLog appender.
-      root {
-        error 'appLog'
-        additivity = true
-      }
-      //warn 'grails.app.service'
-      //warn 'grails.app.controller'
-      break
-  }
+    debug 'grails.app' // Set the default log level for our app code.
+    //error 'grails.app.bootstrap' // Set the log level per type and per type.class
+    //error 'grails.app.service.AuthService'
+    //error 'grails.app.service.NavigationService'
+    //error 'grails.app.service.com.zeddware.grails.plugins.filterpane.FilterService'
+    //info 'org.codehaus.groovy.grails.plugins.searchable'
+    //info 'org.compass'
+    //error 'grails.app.task' // Quartz jobs.
+    //info 'grails.app.task.InventoryIndexJob'
+
+    // Move anything that should behave differently into this section.
+    switch (environment) {
+        case 'development':
+            // Configure the root logger to output to stdout and appLog appenders.
+            root {
+                error 'stdout', 'appLog'
+                additivity = true
+            }
+            //debug "org.hibernate.SQL"
+            //debug 'grails.app.service'
+            //debug 'grails.app.controller'
+            break
+        case 'test':
+            // Configure the root logger to only output to appLog appender.
+            root {
+                error 'stdout', 'appLog'
+                additivity = true
+            }
+            //debug 'grails.app.service'
+            //debug 'grails.app.controller'
+            break
+        case 'production':
+            // Configure the root logger to only output to appLog appender.
+            root {
+                error 'appLog'
+                additivity = true
+            }
+            //warn 'grails.app.service'
+            //warn 'grails.app.controller'
+            break
+    }
 }
 
 // Added by the Spring Security Core plugin:
@@ -179,66 +177,66 @@ if (environment == "production") {
 }
 
 access {
-  google {
-    oauth {
-      key = "everfeeds.com"
-      secret = "mucd4gqA1yLtrY6eMzZo3IYe"
-      provider = org.scribe.builder.api.GoogleApi
+    google {
+        oauth {
+            key = "everfeeds.com"
+            secret = "mucd4gqA1yLtrY6eMzZo3IYe"
+            provider = org.scribe.builder.api.GoogleApi
+        }
+        auth = false
+        emailUrl = "https://www.googleapis.com/userinfo/email"
     }
-    auth = false
-    emailUrl = "https://www.googleapis.com/userinfo/email"
-  }
-  gmail {
-    extend = "google"
-    auth = true
-    oauth {
-      scope = "https://www.googleapis.com/auth/userinfo#email https://mail.google.com/mail/feed/atom/ https://apps-apis.google.com/a/feeds/email_settings/2.0/"
+    gmail {
+        extend = "google"
+        auth = true
+        oauth {
+            scope = "https://www.googleapis.com/auth/userinfo#email https://mail.google.com/mail/feed/atom/ https://apps-apis.google.com/a/feeds/email_settings/2.0/"
+        }
     }
-  }
-  greader {
-    extend = "google"
-    auth = true
-    oauth {
-      scope = "http://www.google.com/reader/api/ http://www.google.com/reader/atom/ https://www.googleapis.com/auth/userinfo#email"
+    greader {
+        extend = "google"
+        auth = true
+        oauth {
+            scope = "http://www.google.com/reader/api/ http://www.google.com/reader/atom/ https://www.googleapis.com/auth/userinfo#email"
+        }
     }
-  }
-  evernote {
-    userAgent = "everfeeds.com"
-    host = "sandbox.evernote.com"
-    oauth {
-      if (environment == "production") {
-        key = "name_alari"
-        secret = "f3ea2d71d1647525"
-        //provider = org.scribe.builder.api.EvernoteApi
-        provider = everfeeds.EvernoteSandboxApi
-      } else {
-        key = "name_alari"
-        secret = "f3ea2d71d1647525"
-        //provider = org.scribe.builder.api.EvernoteApi.Sandbox
-        provider = everfeeds.EvernoteSandboxApi
-      }
+    evernote {
+        userAgent = "everfeeds.com"
+        host = "sandbox.evernote.com"
+        oauth {
+            if (environment == "production") {
+                key = "name_alari"
+                secret = "f3ea2d71d1647525"
+                //provider = org.scribe.builder.api.EvernoteApi
+                provider = everfeeds.EvernoteSandboxApi
+            } else {
+                key = "name_alari"
+                secret = "f3ea2d71d1647525"
+                //provider = org.scribe.builder.api.EvernoteApi.Sandbox
+                provider = everfeeds.EvernoteSandboxApi
+            }
+        }
     }
-  }
-  twitter {
-    oauth {
-      key = 'A5maG2S6WHvloLeFDeIw'
-      secret = '2QFVqw7L0GISHTgdB11GHcmhJo970qRmt2Tg10'
-      provider = org.scribe.builder.api.TwitterApi
+    twitter {
+        oauth {
+            key = 'A5maG2S6WHvloLeFDeIw'
+            secret = '2QFVqw7L0GISHTgdB11GHcmhJo970qRmt2Tg10'
+            provider = org.scribe.builder.api.TwitterApi
+        }
     }
-  }
-  facebook {
-    oauth {
-      key = "118265721567840"
-      secret = "9d43b1e1ce985e1b3f81d44e51e8cd0f"
-      provider = org.scribe.builder.api.FacebookApi
-      scope = "read_stream"
+    facebook {
+        oauth {
+            key = "118265721567840"
+            secret = "9d43b1e1ce985e1b3f81d44e51e8cd0f"
+            provider = org.scribe.builder.api.FacebookApi
+            scope = "read_stream"
+        }
     }
-  }
-  linkedin {
-    oauth {
-      key = "LYuiN2KtQJVJcHOggAZsMT20HzezFqMFvHlAVsaUju5y7gjhc6Y3BJpFLg86QNBX"
-      secret = "p93hwB8RY5g7BaEwXc4qZbZhZ7Zqsxl0Rv4TRV9zcW4dBeNuTCfFv5rwjyACUL1U"
-      provider = org.scribe.builder.api.LinkedInApi
+    linkedin {
+        oauth {
+            key = "LYuiN2KtQJVJcHOggAZsMT20HzezFqMFvHlAVsaUju5y7gjhc6Y3BJpFLg86QNBX"
+            secret = "p93hwB8RY5g7BaEwXc4qZbZhZ7Zqsxl0Rv4TRV9zcW4dBeNuTCfFv5rwjyACUL1U"
+            provider = org.scribe.builder.api.LinkedInApi
+        }
     }
-  }
 }
